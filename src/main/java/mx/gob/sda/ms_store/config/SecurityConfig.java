764e199278d54e253f25a3e1d6f112779956e5ca
Osmel Mendez <84472632+OsmelMdz@users.noreply.github.com>
2026-02-11 11:08:26 -0600

    ms-store v2.5

--- src/main/java/mx/gob/sda/ms_store/config/SecurityConfig.java
+++ src/main/java/mx/gob/sda/ms_store/config/SecurityConfig.java
@@ -4,6 +4,10 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -13,14 +17,22 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http
+            .csrf(csrf -> csrf.disable())
             .x509(x509 -> x509
                 .subjectPrincipalRegex("CN=(.*?)(?:,|$)") 
             )
             .authorizeHttpRequests(auth -> auth
                 .anyRequest().authenticated()
-            )
-            .csrf(csrf -> csrf.disable());
+            );
             
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        return username -> {
+            return new User(username, "", 
+                AuthorityUtils.createAuthorityList("ROLE_USER"));
+        };
     }
 }
