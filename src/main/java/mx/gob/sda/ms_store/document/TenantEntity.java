926aa424cf8ef33bed453e0bd5193ba5918b40e6
Osmel Mendez <84472632+OsmelMdz@users.noreply.github.com>
2026-02-16 08:13:32 -0600

    ms-store v4.1

--- src/main/java/mx/gob/sda/ms_store/document/TenantEntity.java
+++ src/main/java/mx/gob/sda/ms_store/document/TenantEntity.java
@@ -1,15 +1,14 @@
 package mx.gob.sda.ms_store.document;
 
 import jakarta.persistence.*;
-import lombok.Data;
+import lombok.*;
 import java.util.UUID;
 
 @Entity
 @Table(name = "tenants", schema = "catalogos_mirror")
-@Data
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor @Builder
 public class TenantEntity {
     @Id
-    @Column(name = "tenant_id")
     private UUID tenantId;
     private String name;
     private String status;
